4bdc6ced8df85be2838f6045f3e97fbcff6767e9
Mekbib Awono <mekbib.awono@blueprintgenetics.com>
2022-07-20 12:08:19 +0300

    added game won

--- script.js
+++ script.js
@@ -32,16 +32,33 @@ const checkGameOver = () => {
   }
 }
 
+const checkGameWin = () => {
+  const letterEls = document.querySelectorAll('.letter-box');
+  let allAnswered = true;
+  letterEls.forEach(el => {
+    if (el.innerHTML === "&nbsp;") {
+      allAnswered = false;
+    }
+  });
+
+  if (allAnswered) {
+    document.querySelector(".game-won").classList.remove("hide");
+  }
+}
+
 const guessLetter = (e) => {
   const clickedLetter = e.target.textContent;
+  // e.target.removeEventListener("click")
 
   if (word.indexOf(clickedLetter) >= 0) {
     const letterEl = document.querySelectorAll(`.letter-box.letter-${clickedLetter}`);
     letterEl.forEach(el => { el.innerHTML = clickedLetter; });
     e.target.classList.add("right-guess");
+    checkGameWin();
     return false;
   }
 
+  // show the parts of the hangman that were hidden
   const hiddenEls = document.querySelectorAll(".hide");
   hiddenEls[0].classList.remove("hide");
   wrongGuessCount++;
@@ -69,9 +86,10 @@ buildWordBoxes();
 const replay = () => {
   const replayBtn = document.querySelector(".replay");
   replayBtn.addEventListener("click", () => {
-    wrongGuessCount = 0;
-    document.querySelector(".game-over").classList.add("hide");
-  });
+    // wrongGuessCount = 0;
+    // document.querySelector(".game-over").classList.add("hide");
+    location.reload();
+  }); 
 }
 
 replay();
